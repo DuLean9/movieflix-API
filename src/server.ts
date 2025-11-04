a1603821352757d424b34d70b2ea81cb04f7a6d1
DuLean9 <duilioleandro9@outlook.com>
2025-11-03 21:34:22 -0300

    teste dois

--- src/server.ts
+++ src/server.ts
@@ -8,7 +8,7 @@ const prisma = new PrismaClient();
 app.get("/movies", async (req, res) => {
   const movies = await prisma.movie.findMany();
   res.json(movies);
-  console.log("teste");
+  console.log("teste 2");
 });
 
 app.listen(port, () => {
